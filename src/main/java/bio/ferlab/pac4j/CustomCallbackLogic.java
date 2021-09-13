6e0fdff15aa79390c7442d24858bbd8719d43fd9
aodiogo <diogo.oliveira@gmail.com>
2021-09-13 14:17:40 -0400

    SKFP-11: Add CustomCallbackLogic.java

---
+++ src/main/java/bio/ferlab/pac4j/CustomCallbackLogic.java
@@ -0,0 +1,24 @@
+package bio.ferlab.pac4j;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.core.engine.DefaultCallbackLogic;
+import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.core.util.HttpActionHelper;
+import org.pac4j.core.util.Pac4jConstants;
+import org.pac4j.core.exception.http.FoundAction;
+
+public class CustomCallbackLogic extends DefaultCallbackLogic {
+
+    @Override
+    public HttpAction redirectToOriginallyRequestedUrl(final WebContext context, final SessionStore sessionStore,
+                                                          final String defaultUrl) {
+        if(!(defaultUrl == null || defaultUrl.isEmpty() || Pac4jConstants.DEFAULT_URL_VALUE.equals(defaultUrl))) {
+            return HttpActionHelper.buildRedirectUrlAction(context, ( new FoundAction(defaultUrl) ).getLocation());
+        } else {
+            return super.redirectToOriginallyRequestedUrl(context, sessionStore, "");
+        }
+    }
+
+    
+}
