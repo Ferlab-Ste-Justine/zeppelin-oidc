b40bc06745e984def7d433f0c7e66ae96dcf82c6
aodiogo <diogo.oliveira@gmail.com>
2021-09-15 12:01:28 -0400

    SKFP_11: add pac4jRealm.principalNameAttribute on shiro.ini.template

--- src/main/java/bio/ferlab/pac4j/ForceDefaultURLCallbackLogic.java
+++ src/main/java/bio/ferlab/pac4j/ForceDefaultURLCallbackLogic.java
@@ -7,13 +7,15 @@ import org.pac4j.core.exception.http.HttpAction;
 import org.pac4j.core.util.HttpActionHelper;
 import org.pac4j.core.util.Pac4jConstants;
 import org.pac4j.core.exception.http.FoundAction;
+import org.pac4j.core.util.CommonHelper;
 
 public class ForceDefaultURLCallbackLogic extends DefaultCallbackLogic {
 
     @Override
     public HttpAction redirectToOriginallyRequestedUrl(final WebContext context, final SessionStore sessionStore,
                                                           final String defaultUrl) {
-        if(!(defaultUrl == null || defaultUrl.isEmpty() || Pac4jConstants.DEFAULT_URL_VALUE.equals(defaultUrl))) {
+        if(CommonHelper.isNotBlank(defaultUrl) && 
+            !Pac4jConstants.DEFAULT_URL_VALUE.equals(defaultUrl)) {
             return HttpActionHelper.buildRedirectUrlAction(context, ( new FoundAction(defaultUrl) ).getLocation());
         } else {
             return super.redirectToOriginallyRequestedUrl(context, sessionStore, "");
